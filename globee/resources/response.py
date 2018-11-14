fe144d7eec1476277db58346cfeaa8e9c229637d
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2018-11-14 13:06:47 +0100

    move STATUSES dict from request to response

--- globee/resources/response.py
+++ globee/resources/response.py
@@ -4,6 +4,21 @@ from .exceptions import Globee404NotFound, Globee422UnprocessableEntity
 
 
 class GlobeePaymentResponse(Result):
+    STATUSES = {
+        "unpaid": "All payment-requests start in the unpaid state, ready to receive payment.",
+        "paid": "The payment request has been paid, waiting for required number of confirmations.",
+        "underpaid": "Payment has been received, however, the user has paid less than the amount requested. "
+        "This generally should not happen, and is only if the user changed the amount during payment.",
+        "overpaid": "Payment has been received, however, the user has mistakenly paid more than the amount requested. "
+        "This generally should not happen, and is only if the user changed the amount during payment.",
+        "paid_late": "Payment has been received, however, the payment was made outside of the quotation window.",
+        "confirmed": "Payment has been confirmed based on your profile confirmation risk settings.",
+        "completed": "The payment-request is now completed, having reached maximum confirmations, and Globee will start its settling process.",
+        "refunded": "The invoice was refunded and cancelled.",
+        "cancelled": "The invoice was cancelled.",
+        "draft": "Invoice has been saved as a draft and not yet active.",
+    }
+
     def __init__(self, response=None):
         super().__init__()
 
