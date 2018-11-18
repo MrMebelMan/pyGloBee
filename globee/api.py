0fa397f8c06669ba2aa1a61dd94c2fb500b07ad0
Vladyslav Burzakovskyy <vladislav.burzakovskij@satoshilabs.com>
2018-11-18 16:33:00 +0100

    add GlobeePayment class

--- globee/api.py
+++ globee/api.py
@@ -1,6 +1,57 @@
 from globee.resources.request import GlobeePingRequest, GlobeePaymentRequest
 from globee.resources.utils import remove_empty_keys
 from globee.resources.exceptions import GlobeeMissingCredentials
+from decimal import Decimal
+
+
+class GlobeePayment:
+    STATUSES = {
+        "unpaid": "All payment-requests start in the unpaid state, ready to receive payment.",
+        "paid": "The payment request has been paid, waiting for required number of confirmations.",
+        "underpaid": "Payment has been received, however, the user has paid less than the amount requested. "
+        "This generally should not happen, and is only if the user changed the amount during payment.",
+        "overpaid": "Payment has been received, however, the user has mistakenly paid more than the amount requested. "
+        "This generally should not happen, and is only if the user changed the amount during payment.",
+        "paid_late": "Payment has been received, however, the payment was made outside of the quotation window.",
+        "confirmed": "Payment has been confirmed based on your profile confirmation risk settings.",
+        "completed": "The payment-request is now completed, having reached maximum confirmations,\
+                      and Globee will start its settling process.",
+        "refunded": "The invoice was refunded and cancelled.",
+        "cancelled": "The invoice was cancelled.",
+        "draft": "Invoice has been saved as a draft and not yet active.",
+    }
+
+    def __init__(self, json):
+        self.success = json['success']
+        data = json['data']
+        
+        self.adjusted_total = data["adjusted_total"]
+        self.callback_data = data["callback_data"]
+        self.cancel_url = data["cancel_url"]
+        self.confirmation_speed = data["confirmation_speed"]
+        self.created_at = data["created_at"]
+        self.currency = data["currency"]
+        self.custom_payment_id = data["custom_payment_id"]
+        self.custom_store_reference = data["custom_store_reference"]
+        self.expires_at = data["expires_at"]
+        self.id = data["id"]
+        self.ipn_url = data["ipn_url"]
+        self.notification_email = data["notification_email"]
+        self.redirect_url = data["redirect_url"]
+        self.status = data["status"]
+        self.success_url = data["success_url"]
+        self.total = Decimal(data["total"])
+        
+        self.customer_name = data['customer']['name']
+        self.customer_email = data['customer']['email']
+        
+        self.payment_currency = data['payment_details']['currency']
+        self.received_amount = Decimal(data['payment_details']['received_amount'] or '0')
+        self.received_difference = Decimal(data['payment_details']['received_difference'] or '0')
+
+    def __str__(self):
+        return "GloBee Payment #%s (%.2f %s), created: %s, status: %s" \
+               % (self.id, self.total, self.currency, self.created_at, self.status)
 
 
 class Globee:
@@ -64,5 +115,5 @@ class Globee:
             endpoint=self.api_url,
             data=request_data
         )
+        return GlobeePayment(request.response.json)
 
-        return request.response
--- globee/resources/response.py
+++ globee/resources/response.py
@@ -4,20 +4,6 @@ from .exceptions import Globee404NotFound, Globee422UnprocessableEntity
 
 
 class GlobeePaymentResponse(Result):
-    STATUSES = {
-        "unpaid": "All payment-requests start in the unpaid state, ready to receive payment.",
-        "paid": "The payment request has been paid, waiting for required number of confirmations.",
-        "underpaid": "Payment has been received, however, the user has paid less than the amount requested. "
-        "This generally should not happen, and is only if the user changed the amount during payment.",
-        "overpaid": "Payment has been received, however, the user has mistakenly paid more than the amount requested. "
-        "This generally should not happen, and is only if the user changed the amount during payment.",
-        "paid_late": "Payment has been received, however, the payment was made outside of the quotation window.",
-        "confirmed": "Payment has been confirmed based on your profile confirmation risk settings.",
-        "completed": "The payment-request is now completed, having reached maximum confirmations, and Globee will start its settling process.",
-        "refunded": "The invoice was refunded and cancelled.",
-        "cancelled": "The invoice was cancelled.",
-        "draft": "Invoice has been saved as a draft and not yet active.",
-    }
 
     def __init__(self, response=None):
         super().__init__()
@@ -25,12 +11,8 @@ class GlobeePaymentResponse(Result):
         self.errors = []
         self.response = response
         self.status_code = response.status_code
-        self.ok = response.ok
         self.reason = response.reason
-        self.text = response.text
         self.json = response.json()
-        self.redirect_url = ""
-        self.payment_id = ""
 
         if self.status_code == 404:
             raise Globee404NotFound()
@@ -39,6 +21,3 @@ class GlobeePaymentResponse(Result):
             raise Globee422UnprocessableEntity(self.errors)
         elif self.status_code != 200 or not self.json["success"]:
             raise Exception("%s: %s" % (self.response, self.reason))
-
-        self.redirect_url = self.json["data"]["redirect_url"]
-        self.payment_id = self.json["data"]["id"]
